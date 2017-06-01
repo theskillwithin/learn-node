9c9798b06cb7a9034067d14002e4a2637f17738b
Austin Peterson <austinppeterson@gmail.com>
2017-05-31 21:00:13 -0600

    26

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -45,4 +45,13 @@ router.post('/register',
 
 router.get('/logout', authController.logout)
 
+router.get('/account',
+  authController.isLoggedIn,
+  userController.account
+)
+
+router.post('/account',
+  catchErrors(userController.updateAccount)
+)
+
 module.exports = router
