abb1ef5e6190dcd320d1d429f4e62e0ad8e43b6e
Austin Peterson <austinppeterson@gmail.com>
2017-06-21 15:06:38 -0600

    40

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -51,9 +51,30 @@ exports.resize = async (req, res, next) => {
 }
 
 exports.getStores = async (req, res) => {
+  const page = req.params.page || 1
+  const limit = 4
+  const skip = (page * limit) - limit
+
   // 1. query the database for a list of all stores
-  const stores = await Store.find()
-  res.render('stores', { title: 'Stores', stores })
+  const storesPromise = Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' })
+
+  const countPromise = Store.count()
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise])
+
+  const pages = Math.ceil(count / limit)
+
+  if (!stores.length && skip) {
+    req.flash('info', `Hey! you asked for page ${page}.  but that doens't exist. so I put you on page ${pages}`)
+    res.redirect(`/stores/page/${pages}`)
+    return
+  }
+
+  res.render('stores', { title: 'Stores', stores, page, pages, count })
 }
 
 const confirmOwner = (store, user) => {
